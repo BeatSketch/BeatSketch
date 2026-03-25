08922249cc2a615d8b3de4e8504fb3dc98f74114
Janis Hutz <development@janishutz.com>
2026-03-25 11:40:04 +0100

    Add beatmap class (WIP)

--- main.lua
+++ main.lua
@@ -1,3 +1,9 @@
+-- TODO: Make this more reliable/versatile
+package.path = package.path .. ";" .. os.getenv("PWD") .. "/BeatSketch/lua_modules/share/lua/5.4/?.lua"
+
+local map = require("map.main")
+
 function lovr.draw(pass)
 	pass:text("Hello World!", 0, 1.7, -2, 0.5)
+	local beatmap = map:new()
 end
--- map/main.lua
+++ map/main.lua
@@ -1,14 +1,34 @@
-local beatmap = {
-	test = true,
-}
+--- @class BeatMap
+--- @field data table
+local BeatMap = {}
 
-function beatmap:save()
-	self.test = false
-	local file = io.open("main.lua", "a")
-	if file ~= nil then
-		file:write("test")
+local json = require("json")
+
+--- Create a new BeatMap
+--- @return BeatMap
+function BeatMap:new()
+	local o = {
+		data = {},
+	}
+	setmetatable(o, self)
+	self.__index = self
+	return o
+end
+
+--- Save the BeatMap to disk
+--- @param path string The file path to save to
+function BeatMap:save(path)
+	local file = io.open(path, "w")
+	if file then
+		file:write(json.encode(self.data))
 		file:close()
 	end
 end
 
-return beatmap
+--- Add a new block
+--- @return string
+function BeatMap:add_block()
+	return ""
+end
+
+return BeatMap
