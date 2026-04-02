8d0192a02af9361af8e217562a4b9d8f0a9053e7
Janis Hutz <development@janishutz.com>
2026-04-02 15:37:58 +0200

    prepare tracking handling

--- main.lua
+++ main.lua
@@ -1,9 +1,42 @@
--- TODO: Make this more reliable/versatile
-package.path = package.path .. ";" .. os.getenv("PWD") .. "/BeatSketch/lua_modules/share/lua/5.4/?.lua"
+--[[
+ ___               _   ___   _           _         _
+(  _ \            ( )_(  _ \( )         ( )_      ( )
+| (_) )  __    _ _|  _) (_(_) |/ )   __ |  _)  ___| |__
+|  _ ( / __ \/ _  ) |  \__ \|   (  / __ \ |  / ___)  _  \
+| (_) )  ___/ (_| | |_( )_) | |\ \(  ___/ |_( (___| | | |
+(____/ \____)\__ _)\__)\____)_) (_)\____)\__)\____)_) (_)
 
-local map = require("map.beatmap")
+--]]
 
+-- FIXME: Make this more reliable/versatile
+-- TODO: Consider just putting the library in here
+local lua_version = _VERSION:sub(5)
+local cwd = os.getenv("PWD")
+package.path = package.path .. ";" .. cwd .. "/BeatSketch/lua_modules/share/lua/" .. lua_version .. "/?.lua"
+
+-- ┌                                               ┐
+-- │                 Configuration                 │
+-- └                                               ┘
+function lovr.conf(t)
+	-- We can also start the headset session later on (by calling lovr.headset.start and do a desktop UI first)
+	-- I have yet to figure out the resizing and stuff
+	t.headset.start = true
+	t.window.resizable = true
+end
+
+-- ┌                                               ┐
+-- │                    Drawing                    │
+-- └                                               ┘
+-- Drawing the screen is called once every frame
 function lovr.draw(pass)
 	pass:text("Hello World!", 0, 1.7, -2, 0.5)
-	local beatmap = map:new()
+end
+
+-- ┌                                               ┐
+-- │              Physics / Tracking               │
+-- └                                               ┘
+-- Tracking and the like get continuous updates
+local tracking = require("tracking.main")
+function lovr.update(delta_time)
+	tracking.tracker(delta_time)
 end
---
+++ tracking/main.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+--- Run the controller and headset tracker
+---@param delta_time number time since last update
+M.tracker = function(delta_time) end
+
+return M
